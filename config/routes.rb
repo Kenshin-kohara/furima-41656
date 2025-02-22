8e3e3372cae6fc5ded0ea56ad1e1068d6f3e87e3
Kenshin-kohara <ken17.ssbulu@gmail.com>
2025-02-22 18:07:32 +0900

    新規登録ボタンの誘導、コントローラー生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: "items#index"
+  resources :users, only: [:index, :create, :destroy]
 end
