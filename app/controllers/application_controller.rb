ae5453b75392a57b3a0c2f2b2eea89184bbb2926
Kenshin-kohara <ken17.ssbulu@gmail.com>
2025-02-26 22:09:21 +0900

    以後の編集において、userモデルにnicknameカラムを追加し、DBへ保存されるよう編集する。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     end
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_read, :first_name_read, :date_of_birth])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :last_name, :first_name, :last_name_read, :first_name_read, :date_of_birth])
     end
 
   end
