272b552dc249e5a8f7a1252a647540ce0cdbfae4
Kenshin-kohara <ken17.ssbulu@gmail.com>
2025-02-26 19:35:42 +0900

    新規登録ページ、ログインページへの誘導、バリデーションの一部記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :email, :encrypted_password, :last_name, :first_name,
+            :last_name_read, :first_name_read, :date_of_birth, presence: true
 end
