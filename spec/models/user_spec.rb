90f1da5e42d08e206678f028766e81dc4f757a9b
Kenshin-kohara <ken17.ssbulu@gmail.com>
2025-02-28 14:47:41 +0900

    コメントの削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,43 +9,43 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user = FactoryBot.build(:user)
       user.email = '' 
       user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
     end
     it 'パスワードが空では保存できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user = FactoryBot.build(:user)
       user.password = ''  
       user.valid?
       expect(user.errors.full_messages).to include("Password can't be blank")
     end
     it 'last_nameが空では保存できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user = FactoryBot.build(:user)
       user.last_name = ''  
       user.valid?
       expect(user.errors.full_messages).to include("Last name can't be blank")
     end
     it 'first_nameが空では保存できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user = FactoryBot.build(:user)
       user.first_name = ''  
       user.valid?
       expect(user.errors.full_messages).to include("First name can't be blank")
     end
     it 'last_name_readが空では保存できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user = FactoryBot.build(:user)
       user.last_name_read = ''  
       user.valid?
       expect(user.errors.full_messages).to include("Last name read can't be blank")
     end
     it 'first_name_readが空では保存できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user = FactoryBot.build(:user)
       user.first_name_read = ''  
       user.valid?
       expect(user.errors.full_messages).to include("First name read can't be blank")
     end
     it 'date_of_birthが空では保存できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user = FactoryBot.build(:user)
       user.date_of_birth = ''  
       user.valid?
       expect(user.errors.full_messages).to include("Date of birth can't be blank")
