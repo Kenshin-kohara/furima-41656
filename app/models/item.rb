3cf2ae22995f6487d3820ba45810855ab95138f8
Kenshin-kohara <ken17.ssbulu@gmail.com>
2025-03-03 19:35:19 +0900

    各選択項目、アクティブハッシュの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
-  
+
   has_one_attached :image
   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  validates :category_id, :keep_id, :bearer_id, :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank" }
 end
