dacfac7a7dcf93b3bcfea02c63becd3a4d7218cd
Kenshin-kohara <ken17.ssbulu@gmail.com>
2025-03-04 16:02:26 +0900

    出品画面の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,15 @@
 class ItemsController < ApplicationController
 
+  def new
+    @item = Item.new
+  end
+
   private
 
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
+  
   
 
 end
