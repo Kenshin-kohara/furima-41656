197fbcda0e69ccd62d9d77530294274b38b2fc34
Kenshin-kohara <ken17.ssbulu@gmail.com>
2025-03-15 21:26:27 +0900

    修正3

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :keep
   belongs_to :prefecture
-  belongs_to :shipping_day_id
+  belongs_to :shipping_day
   belongs_to :bearer
 
   validates :image, :name, :explanation, presence: true
