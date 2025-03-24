185cdf6ba4540d031ae44f1410586e468c96cdd0
Kenshin-kohara <ken17.ssbulu@gmail.com>
2025-03-24 12:30:15 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
@@ -21,17 +21,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-     redirect_to root_path(@item)
+     redirect_to item_path(@item)
     else
       render :edit, status: :unprocessable_entity
     end
