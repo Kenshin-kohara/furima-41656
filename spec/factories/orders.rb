27c9b3fa9711f77a798a897e720c4ef552f759e1
Kenshin-kohara <ken17.ssbulu@gmail.com>
2025-06-03 04:21:24 +0900

    テストコード　途中まで

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,13 +1,10 @@
 FactoryBot.define do
   factory :order do
-    post_code         {'023-0234'}
-    prefecture_id     {2}
-    city              {'cityテスト'}
-    street            {'streetテスト'}
-    building          {'buildingテスト'}
-    phone_number      {'02302340234'}
-    association :user
-    association :item
-    association :purchase_record
+    post_code         { '023-0234' }
+    prefecture_id     { 2 }
+    city              { 'cityテスト' }
+    street            { 'streetテスト' }
+    building          { 'buildingテスト' }
+    phone_number      { '02302340234' }
   end
 end
--- spec/models/order.spec.rb
+++ spec/models/order.spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
-RSpec.describe Item, type: :model do
+RSpec.describe Order, type: :model do
   before do
-    @user1 = FactoryBot.create(:user) # ユーザー1を登録
-    @item = FactoryBot.build(:item, user: @user1) #ユーザー1を出品者に紐づけ
-    @user2 = FactoryBot.create(:user) # ユーザー2を登録
-
+    user = FactoryBot.create(:user) # ユーザー1を登録
+    item = FactoryBot.build(:item, user_id: 1) #ユーザー1を出品者に紐づけ
+    user = FactoryBot.create(:user) # ユーザー2を登録
+    @order = FactoryBot.build(:order, user_id: user.id, item_id: item.id)
   end
 
   describe '商品購入機能' do
     context '商品購入できるとき' do
-    @order = FactoryBot.build(:order, user: @user2, item: @item) #注文にはユーザー2と商品を紐づけ
-      #ユーザー2でログイン
       it'6つの項目が埋まっていれば購入ができる' do
         expect(@order).to be_valid
       end
@@ -22,21 +20,41 @@ RSpec.describe Item, type: :model do
     end
 
     context '商品購入ができないとき' do
-      @order = FactoryBot.build(:order, user: @user2, item: @item) #注文にはユーザー2と商品を紐づけ
-      #ユーザー2でログイン
-      it '空欄だと購入できない' do
+      it '郵便番号が空欄だと購入できない' do
         @order.post_code = ''
         @order.valid?
-        expect(@order.errors.full_messages).to include("post_code can't be blank")
+        expect(@order.errors.full_messages).to include("Post code can't be blank")
       end
 
-      #その他購入ができないテスト
+      it '都道府県がデフォルトだと購入できない' do
+        @order.prefecture_id = 0
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '市区町村が空欄だと購入できない' do
+        @order.city = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("City can't be blank")
+      end
+
+      it '番地が空欄だと購入できない' do
+        @order.street = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Street can't be blank")
+      end
+
+      it '電話番号が空欄だと購入できない' do
+        @order.phone_number = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number can't be blank")
+      end
 
     end
 
     context '自分の商品を購入できない' do
       it '自分の出品した商品は購入できない' do
-        @order = FactoryBot.build(:order, user: @user1, item: @item)
+        @order = FactoryBot.build(:order, user_id: 1, item_id: item.id)
         expect(@order).not_to be_valid
       end
     end
@@ -44,7 +62,7 @@ RSpec.describe Item, type: :model do
     context 'ログインしなければ商品を購入できない' do
       it 'ログインしなければ商品を購入できない' do
         expect(@order).not_to be_valid
-        #ログインが無い状況で商品を購入しようとし、それが不可能になるテスト
+
       end
     end
 
